d0c1d2b9f5276e1c9b0277431e149108bf26fc01
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-09-02 16:57:40 +0200

    Update ids.h

--- kernel-ids/ids.h
+++ kernel-ids/ids.h
@@ -1,1 +1,28 @@
 
+#ifndef IDS_H
+#define IDS_H
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/syscalls.h>
+#include <linux/kprobes.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/sched.h>
+#include <asm/stackprotector.h>  // Stack canary
+
+extern unsigned long *syscall_table;
+extern struct nf_hook_ops netfilter_ops;
+extern struct jprobe jp;
+
+void monitor_network_traffic(void);
+void detect_stack_canary(void);
+void check_syscall_table(void);
+void check_hidden_processes(void);
+
+#endif // IDS_H
