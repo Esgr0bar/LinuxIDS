d0c4c16a192a6f58450fe1e86a79ae9108beec08
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-09-02 16:59:22 +0200

    Update rootkit_detection.c

--- kernel-ids/rootkit_detection.c
+++ kernel-ids/rootkit_detection.c
@@ -1,1 +1,18 @@
 
+#include "ids.h"
+
+/* Function to check syscall table integrity */
+void check_syscall_table(void) {
+    syscall_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+
+    if (!syscall_table) {
+        printk(KERN_ERR "IDS: Unable to locate syscall table\n");
+    } else {
+        unsigned long original_syscall = syscall_table[__NR_open];
+        if (syscall_table[__NR_open] != (unsigned long)sys_open) {
+            printk(KERN_ALERT "IDS: Syscall table modification detected! Possible rootkit!\n");
+        } else {
+            printk(KERN_INFO "IDS: Syscall table integrity verified.\n");
+        }
+    }
+}
