81aa68ca05358e279b3443d8532d8f43ad47534c
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-09-03 09:27:48 +0200

    Update ids.h

--- kernel-ids/ids.h
+++ kernel-ids/ids.h
@@ -12,20 +12,21 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
-#include <linux/bpf.h>
-#include <linux/ptrace.h>
-#include <linux/if_packet.h>
-#include <linux/pf_ring.h>
+#include <linux/sched.h>
+#include <linux/ebpf.h>  // eBPF support
+#include <pf_ring.h>     // PF_RING support
 #include <asm/stackprotector.h>  // Stack canary
 
 extern unsigned long *syscall_table;
 extern struct nf_hook_ops netfilter_ops;
 extern struct jprobe jp;
 
+/* Function prototypes */
 void monitor_network_traffic(void);
 void detect_stack_canary(void);
 void check_syscall_table(void);
 void check_hidden_processes(void);
-int load_ebpf_program(void);
+void setup_ebpf_programs(void);
+void pf_ring_setup(void);
 
 #endif // IDS_H
